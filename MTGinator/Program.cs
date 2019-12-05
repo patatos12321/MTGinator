9376d49fa0eef0de4c1975f12ee7111c8498bf85
Christian Artin <gravufo@gmail.com>
2019-12-05 18:28:13 -0500

    Enable HTTPS (#5)
    * Add missing nodejs/npm
    
    * Generate self-signed certificate dynamically
    
    * Fix https setup

--- MTGinator/Program.cs
+++ MTGinator/Program.cs
@@ -1,16 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MTGinator
 {
     public class Program
     {
+        private static X509Certificate2 _cert;
+
+        private static X509Certificate2 GetSelfSignedCertificate()
+        {
+            if(_cert != null)
+                return _cert;
+            
+            var distinguishedName = new X500DistinguishedName($"CN=MTGinatorDev");
+
+            using (var rsa = RSA.Create(2048))
+            {
+                var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256,RSASignaturePadding.Pkcs1);
+
+                request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DataEncipherment | X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature , false));
+
+
+                request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(new OidCollection { new Oid("1.3.6.1.5.5.7.3.1") }, false));
+
+                var certificate= request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+
+                // Generate semi random password
+                var password = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+                _cert = new X509Certificate2(certificate.Export(X509ContentType.Pfx, password), password, X509KeyStorageFlags.MachineKeySet);
+            }
+
+            return _cert;
+        }
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -18,6 +43,13 @@ namespace MTGinator
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(kestrelOptions =>
+                {
+                    kestrelOptions.ConfigureHttpsDefaults(listenOptions =>
+                    {
+                        listenOptions.ServerCertificate = GetSelfSignedCertificate();
+                    });
+                })
                 .UseStartup<Startup>();
     }
 }
