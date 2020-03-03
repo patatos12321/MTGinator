a2d183c0893f06b6e3ea0b81ab1fea29f746adca
patatos12321 <31021380+patatos12321@users.noreply.github.com>
2020-03-03 09:29:25 -0500

    Add api for results, rounds and the db (#10)
    * add a controller for results
    
    * add a call to download the database

---
+++ MTGinator/Controllers/DatabaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MTGinator.Controllers
+{
+    [ApiController]
+    [Route("api/db")]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DatabaseController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public FileResult DownloadDb()
+        {
+            var fileBytes = System.IO.File.ReadAllBytes(_config["DatabasePath"]);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, "mtg.db");
+        }
+    }
+}
---
+++ MTGinator/Controllers/ResultsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MTGinator.Repositories;
+using MTGinator.Models;
+
+namespace MTGinator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResultsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+        private readonly IResultRepository _resultRepository;
+
+        public ResultsController(ILogger<EventsController> logger, IResultRepository resultRepository)
+        {
+            _logger = logger;
+            _resultRepository = resultRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Result> Get()
+        {
+            var results = _resultRepository.GetAll();
+            return results;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public Result Get(int id)
+        {
+            var result = _resultRepository.GetById(id);
+            return result;
+        }
+
+        [HttpPost]
+        public Result Post(Result result)
+        {
+            _resultRepository.Save(result);
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            _resultRepository.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public Result Put(Result result)
+        {
+            _resultRepository.Save(result);
+            return result;
+        }
+    }
+}
